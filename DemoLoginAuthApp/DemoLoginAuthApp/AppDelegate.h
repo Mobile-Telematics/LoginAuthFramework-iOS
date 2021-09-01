864cc8f2d06867ace261f83eed44b712da5ee25b
pavel <pp@datamotion.ai>
2021-09-01 13:05:18 +0300

    LoginAuth Framework Demo app /September 1, 2021/

---
+++ DemoLoginAuthApp/DemoLoginAuthApp/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  DemoLoginAuthApp
+//
+//  Created by Damoov on 01.09.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
